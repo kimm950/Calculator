ef7328b36249fc0b0fbc949870f09e1cbd9c5f66
Kimm <hyunsung.kim@zeals.co.jp>
2020-04-18 22:44:19 +0900

    set up basic components and add styled components

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from './node_modules/react'
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default Calculator
