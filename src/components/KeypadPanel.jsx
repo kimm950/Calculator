4bd73b7d1039b4db5f2c70a58c90830bb2ee754d
Kimm <hyunsung.kim@zeals.co.jp>
2020-05-06 12:47:00 +0900

    create button set

--- src/components/KeypadPanel.jsx
+++ src/components/KeypadPanel.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Keypad = styled.div`
+  padding: 10px;
+  button {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    margin: 5px;
+  }
+`
 
 const KeypadPanel = ({ onClick }) => {
   return (
-    <div className="Keypad" />
+    <Keypad>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+      <button onClick={e => onClick(e.target.name)}>C</button>
+    </Keypad>
   );
 }
 
-export default KeypadPanel;
+export default KeypadPanel; 
