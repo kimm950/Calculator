443ab48e2d08514fb4393036d13aca28fa50bb0d
Kimm <hyunsung.kim@zeals.co.jp>
2020-05-06 15:35:42 +0900

    add line breaker

--- src/components/DisplayPanel.jsx
+++ src/components/DisplayPanel.jsx
@@ -5,7 +5,13 @@ const Header = styled.div`
   height: 2vh;
 `
 const Display = styled.div`
+  padding: 10px;
+  max-width: 300px;
   height: 20vh;
+  word-wrap: break-word;
+  color: #ffff;
+  font-weight: bold;
+  font-size: 32px;
   background-color: #475862;
 `
 
