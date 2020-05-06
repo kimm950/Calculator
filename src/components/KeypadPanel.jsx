d3adb9e547c601a951255b229a651ba4a70af745
Kimm <hyunsung.kim@zeals.co.jp>
2020-05-06 15:01:50 +0900

    provides name of each keys

--- src/components/KeypadPanel.jsx
+++ src/components/KeypadPanel.jsx
@@ -37,30 +37,30 @@ const Keypad = styled.div`
 const KeypadPanel = ({ onClick }) => {
   return (
     <Keypad>
-      <button className='basic-row' onClick={e => onClick(e.target.name)}>C</button>
-      <button className='basic-row' onClick={e => onClick(e.target.name)}>(</button>
-      <button className='basic-row' onClick={e => onClick(e.target.name)}>)</button>
-      <button className='symbol' onClick={e => onClick(e.target.name)}>%</button>
+      <button name='C' className='basic-row' onClick={e => onClick(e.target.name)}>C</button>
+      <button name='(' className='basic-row' onClick={e => onClick(e.target.name)}>(</button>
+      <button name=')' className='basic-row' onClick={e => onClick(e.target.name)}>)</button>
+      <button name='/' className='symbol' onClick={e => onClick(e.target.name)}>÷</button>
 
-      <button className='number' onClick={e => onClick(e.target.name)}>1</button>
-      <button className='number' onClick={e => onClick(e.target.name)}>2</button>
-      <button className='number' onClick={e => onClick(e.target.name)}>3</button>
-      <button className='symbol' onClick={e => onClick(e.target.name)}>x</button>
+      <button name='1' className='number' onClick={e => onClick(e.target.name)}>1</button>
+      <button name='2' className='number' onClick={e => onClick(e.target.name)}>2</button>
+      <button name='3' className='number' onClick={e => onClick(e.target.name)}>3</button>
+      <button name='*' className='symbol' onClick={e => onClick(e.target.name)}>x</button>
 
-      <button className='number' onClick={e => onClick(e.target.name)}>4</button>
-      <button className='number' onClick={e => onClick(e.target.name)}>5</button>
-      <button className='number' onClick={e => onClick(e.target.name)}>6</button>
-      <button className='symbol' onClick={e => onClick(e.target.name)}>-</button>
+      <button name='4' className='number' onClick={e => onClick(e.target.name)}>4</button>
+      <button name='5' className='number' onClick={e => onClick(e.target.name)}>5</button>
+      <button name='6' className='number' onClick={e => onClick(e.target.name)}>6</button>
+      <button name='-' className='symbol' onClick={e => onClick(e.target.name)}>-</button>
 
-      <button className='number' onClick={e => onClick(e.target.name)}>7</button>
-      <button className='number' onClick={e => onClick(e.target.name)}>8</button>
-      <button className='number' onClick={e => onClick(e.target.name)}>9</button>
-      <button className='symbol' onClick={e => onClick(e.target.name)}>+</button>
+      <button name='7' className='number' onClick={e => onClick(e.target.name)}>7</button>
+      <button name='8' className='number' onClick={e => onClick(e.target.name)}>8</button>
+      <button name='9' className='number' onClick={e => onClick(e.target.name)}>9</button>
+      <button name='+' className='symbol' onClick={e => onClick(e.target.name)}>+</button>
 
-      <button className='number' onClick={e => onClick(e.target.name)}>.</button>
-      <button className='number' onClick={e => onClick(e.target.name)}>0</button>
-      <button className='number' onClick={e => onClick(e.target.name)}>⬅︎</button>
-      <button className='symbol' onClick={e => onClick(e.target.name)}>=</button>
+      <button name='.' className='number' onClick={e => onClick(e.target.name)}>.</button>
+      <button name='0' className='number' onClick={e => onClick(e.target.name)}>0</button>
+      <button name='CE' className='number' onClick={e => onClick(e.target.name)}>⬅︎</button>
+      <button name='=' className='symbol' onClick={e => onClick(e.target.name)}>=</button>
     </Keypad>
   );
 }
